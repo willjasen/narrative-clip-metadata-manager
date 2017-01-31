918f23d76cfc0702a92c6eac9f1f0eb90fda4def
Brandon <willjasen@gmail.com>
2017-01-31 15:54:15 -0500

    using glob instead of bulk

--- manage.js
+++ manage.js
@@ -1,25 +1,29 @@
 var fs = require('fs');
 var archiver = require('archiver');
 
-var srcDirectory = '/Users/willjasen/Desktop/metadata';
+var srcDirectory = '/Users/willjasen/Pictures/Narrative Clip';
+var dstFilename = '/Users/willjasen/Pictures/metadata';
 var fileExtensions = ['.json','.snap.bin']
 
 fileExtensions.forEach(function(extension,index,array) {
   console.time(extension);
   var zipArchive = archiver('zip');
-  var outputPath = srcDirectory + extension + '.zip';
+  var outputPath = dstFilename + extension + '.zip';
   var output = fs.createWriteStream(outputPath);
 
   output.on('close', function() {
     console.log('Done with the zip:', outputPath);
+    console.log(zipArchive.pointer() + ' total bytes');
     console.timeEnd(extension);
   });
 
   zipArchive.pipe(output);
 
-  zipArchive.bulk([
-    { src: [ '**/*' + extension ], cwd: srcDirectory, expand: true }
-  ]);
+  //zipArchive.bulk([
+  //  { src: [ '**/*' + extension ], cwd: srcDirectory, expand: true }
+  //]);
+
+  zipArchive.glob(srcDirectory + '/**/*' + extension);
 
   zipArchive.finalize(function(err, bytes) {
 
@@ -31,7 +35,5 @@ fileExtensions.forEach(function(extension,index,array) {
 
   });
 });
-
-
 
 // https://soledadpenades.com/2014/01/22/compressing-files-with-node-js/
