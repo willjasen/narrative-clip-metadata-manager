2130108b192e41814241dadf936b67c7f2a8fe37
willjasen <willjasen@gmail.com>
2017-01-30 19:10:00 -0500

    specifying outputPath, gather only .json, adding source

--- manage.js
+++ manage.js
@@ -1,20 +1,21 @@
 var fs = require('fs');
 var archiver = require('archiver');
-var outputPath = "test.zip"
+
+var outputPath = "/Users/willjasen/Desktop/test.zip"
 var srcDirectory = "/Users/willjasen/Desktop/metadata"
 
 var output = fs.createWriteStream(outputPath);
 var zipArchive = archiver('zip');
 
 output.on('close', function() {
-    console.log('done with the zip', outputPath);
+    console.log('Done with the zip: ', outputPath);
 });
 
 zipArchive.pipe(output);
 
 zipArchive.bulk([
-    { src: [ '**/*' ], cwd: srcDirectory, expand: true }
-]);
+    { src: [ '**/*.json' ], cwd: srcDirectory, expand: true }
+])
 
 zipArchive.finalize(function(err, bytes) {
 
@@ -22,6 +23,8 @@ zipArchive.finalize(function(err, bytes) {
       throw err;
     }
 
-    console.log('done:', base, bytes);
+    console.log('Done:', base, bytes);
 
 });
+
+// https://soledadpenades.com/2014/01/22/compressing-files-with-node-js/
