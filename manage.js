c334e22add44b2de5daf11e6840da3ade88fa73d
willjasen <willjasen@gmail.com>
2017-01-30 20:34:55 -0500

    create zip archive of each file type

--- manage.js
+++ manage.js
@@ -1,23 +1,26 @@
 var fs = require('fs');
 var archiver = require('archiver');
 
-var outputPath = "/Users/willjasen/Desktop/test.zip"
-var srcDirectory = "/Users/willjasen/Desktop/metadata"
+var srcDirectory = '/Users/willjasen/Desktop/metadata';
+//var outputPaths = ['/Users/willjasen/Desktop/metadata.json.zip', '/Users/willjasen/Desktop/metadata.snap.bin.zip'];
+var fileExtensions = ['.json','.snap.bin']
 
-var output = fs.createWriteStream(outputPath);
-var zipArchive = archiver('zip');
+fileExtensions.forEach(function(extension,index,array) {
+  var zipArchive = archiver('zip');
+  var outputPath = srcDirectory + extension + '.zip';
+  var output = fs.createWriteStream(outputPath);
 
-output.on('close', function() {
-    console.log('Done with the zip: ', outputPath);
-});
+  output.on('close', function() {
+    console.log('Done with the zip:', outputPath);
+  });
 
-zipArchive.pipe(output);
+  zipArchive.pipe(output);
 
-zipArchive.bulk([
-    { src: [ '**/*.json' ], cwd: srcDirectory, expand: true }
-])
+  zipArchive.bulk([
+    { src: [ '**/*' + extension ], cwd: srcDirectory, expand: true }
+  ]);
 
-zipArchive.finalize(function(err, bytes) {
+  zipArchive.finalize(function(err, bytes) {
 
     if(err) {
       throw err;
@@ -25,6 +28,7 @@ zipArchive.finalize(function(err, bytes) {
 
     console.log('Done:', base, bytes);
 
+  });
 });
 
 // https://soledadpenades.com/2014/01/22/compressing-files-with-node-js/
