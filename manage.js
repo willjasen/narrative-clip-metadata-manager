62d4fa16415284ad35e74cc2167523f680f65d93
willjasen <willjasengmail.com>
2017-02-07 17:11:54 -0500

    stashing changes for removing files after

--- manage.js
+++ manage.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 var archiver = require('archiver');
 
-var srcDirectory = '/Users/willjasen/Pictures/Narrative Clip';
-var dstFilename = '/Users/willjasen/Pictures/metadata';
+var srcDirectory = '/Users/willjasen/Desktop/testnode';
+var dstFilename = '/Users/willjasen/Desktop/metadata';
 var fileExtensions = ['.json','.snap.bin']
 var timestamp = Date.now();
 
+// Compress each file type into its archive
 fileExtensions.forEach(function(extension,index,array) {
   console.time(extension);
   var zipArchive = archiver('zip');
@@ -16,6 +17,29 @@ fileExtensions.forEach(function(extension,index,array) {
     console.log('Done with the zip:', outputPath);
     console.log(zipArchive.pointer() + ' total bytes');
     console.timeEnd(extension);
+
+    var glob = require('glob');
+
+    // Delete the files of each file type
+    fileExtensions.forEach(function(extension,index,array) {
+
+    glob(srcDirectory + '/**/*' + extension,function(err,files){
+
+     if (err) throw err;
+     files.forEach(function(item,index,array){
+          console.log(item + " found");
+     });
+
+     // Delete files
+     files.forEach(function(item,index,array){
+          fs.unlink(item, function(err){
+               if (err) throw err;
+               console.log(item + " deleted");
+           });
+         });
+      });
+    });    
+
   });
 
   zipArchive.pipe(output);
