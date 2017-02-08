28ad49863724c43d2b7822b20b74f72a5e996f99
willjasen <willjasen@gmail.com>
2017-02-08 18:41:02 -0500

    cleaning up

--- manage.js
+++ manage.js
@@ -8,26 +8,24 @@ var timestamp = Date.now();
 
 // Compress each file type into its archive
 fileExtensions.forEach(function(extension,index,array) {
+  
   console.time(extension);
   var zipArchive = archiver('zip');
   var outputPath = dstFilename + '-' + timestamp + extension + '.zip';
   var output = fs.createWriteStream(outputPath);
 
+  // Show how long it took when finished
   output.on('close', function() {
     console.log('Done with the zip:', outputPath);
     console.log(zipArchive.pointer() + ' total bytes');
     console.timeEnd(extension);    
   
-    var glob = require('glob');
-
-
-    glob(srcDirectory + '/**/*' + extension,function(err,files){
+  // Get all files of file type
+  var glob = require('glob');
+  glob(srcDirectory + '/**/*' + extension,function(err,files){
 
      if (err) throw err;
-     files.forEach(function(item,index,array){
-          console.log(item + " found");
-     });
-
+     
      // Delete files
      files.forEach(function(item,index,array){
           fs.unlink(item, function(err){
